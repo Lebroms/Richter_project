0346ed66a7b5af0beea1fadf32fc6fe97d46762c
Lebroms <emagia2002@gmail.com>
2025-07-18 21:35:55 +0200

    Corretto early stopping

--- scripts/LightGBM.py
+++ scripts/LightGBM.py
@@ -42,7 +42,7 @@ class LGBM:
             callbacks=[lgb.early_stopping(stopping_rounds=40)]
         )
         dump(model, model_path)
-        print(f"✅ Modello LGBM salvato in {model_path}")
+        print(f"Modello LGBM salvato in {model_path}")
         return model
 
     @staticmethod
@@ -53,7 +53,7 @@ class LGBM:
     @staticmethod
     def evaluate_f1_micro(y_true, y_pred):
         f1_micro = f1_score(y_true, y_pred, average='micro')
-        print(f"🎯 F1-micro: {f1_micro:.4f}")
+        print(f"F1-micro: {f1_micro:.4f}")
         return f1_micro
 
     @staticmethod
@@ -64,37 +64,37 @@ class LGBM:
         f1_scores = []
 
         for fold in range(1, n_folds + 1):
-            print(f"\n🔁 Fold {fold}")
-
-            # 🔹 Caricamento indici
+            print(f"Fold {fold}")
+            
+            # Caricamento indici
             train_idx = pd.read_csv(f"{path_dir}/fold_{fold}_train.csv", header=None)[0].values
             val_idx = pd.read_csv(f"{path_dir}/fold_{fold}_val.csv", header=None)[0].values
 
-            # 🔹 Creazione dei DataFrame per training e validazione
+            # Creazione dei DataFrame per training e validazione
             df_train = df_full.iloc[train_idx].reset_index(drop=True)
             df_val = df_full.iloc[val_idx].reset_index(drop=True)
 
-            # 🔹 Colonne da eliminare (es. geo_level_1_id e geo_level_3_id)
+            # Colonne da eliminare (es. geo_level_1_id e geo_level_3_id)
             # drop_cols = ['geo_level_1_id', 'geo_level_3_id']
             #categorical_cols = df_train.select_dtypes(include='category').columns.tolist()
 
             # df_train = LGBM.preprocess_dataframe(df_train, drop_cols=drop_cols, categorical_cols=categorical_cols)
             # df_val = LGBM.preprocess_dataframe(df_val, drop_cols=drop_cols, categorical_cols=categorical_cols)
 
-            # 🔹 Addestramento
+            # Addestramento
             path_dir_model = "C:/Users/emagi/Documents/richters_predictor/models/"
             model_path = f"{path_dir_model}/lgbm_model_fold_{fold}.joblib"
             LGBM.train_and_save_model(df_train, df_val, target_col, model_path=model_path)
 
-            # 🔹 Valutazione
+            # Valutazione
             X_val = df_val.drop(columns=[target_col])
             y_val = df_val[target_col]
             y_pred = LGBM.load_and_predict(model_path, X_val)
             f1 = LGBM.evaluate_f1_micro(y_val, y_pred)
             f1_scores.append(f1)
 
-            print(f"📁 Fold {fold} completato.")
+            print(f"Fold {fold} completato.")
 
         mean_f1 = sum(f1_scores) / len(f1_scores)
-        print(f"\n📊 F1-micro media su {n_folds} fold: {mean_f1:.4f}")
+        print(f"F1-micro media su {n_folds} fold: {mean_f1:.4f}")
         return f1_scores, mean_f1
